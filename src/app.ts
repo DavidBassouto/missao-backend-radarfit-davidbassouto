8d977df927b2332d3467aa1d0a7d5fab5729645c
DavidBassouto <davidbassouto@hotmail.com>
2022-11-21 23:38:32 -0300

    feat: error handlind middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import "express-async-errors";
+import { handleErrorMiddleware } from "./middlewares/handleError.middleware";
 import { productRoutes } from "./routes/products.router";
 
 const app = express();
 app.use(express.json());
 
 app.use("/produtos", productRoutes);
+
+app.use(handleErrorMiddleware);
 
 export default app;

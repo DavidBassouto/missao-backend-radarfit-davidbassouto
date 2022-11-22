1ed02b80af5280e95e9a15f21b08dcef83673890
DavidBassouto <davidbassouto@hotmail.com>
2022-11-22 16:01:17 -0300

    fix: change to dist/src/*.entity.js

--- src/data-source.ts
+++ src/data-source.ts
@@ -10,22 +10,22 @@ const AppDataSource = new DataSource(
         entities: ["src/entities/*.ts"],
       }
     : {
-      type: "postgres",
-      url: process.env.DATABASE_URL,
-      ssl:
-        process.env.NODE_ENV === "production"
-          ? { rejectUnauthorized: false }
-          : false,
-      logging: true,
-      synchronize: false,
-      entities:
-        process.env.NODE_ENV === "production"
-          ? ["dist/src/entities/*.js"]
-          : ["src/entities/*.entity.ts"],
-      migrations:
-        process.env.NODE_ENV === "production"
-          ? ["dist/src/migrations/*.js"]
-          : ["src/migrations/*.ts"],
+        type: "postgres",
+        url: process.env.DATABASE_URL,
+        ssl:
+          process.env.NODE_ENV === "production"
+            ? { rejectUnauthorized: false }
+            : false,
+        logging: true,
+        synchronize: false,
+        entities:
+          process.env.NODE_ENV === "production"
+            ? ["dist/src/entities/*.entity.js"]
+            : ["src/entities/*.entity.ts"],
+        migrations:
+          process.env.NODE_ENV === "production"
+            ? ["dist/src/migrations/*.js"]
+            : ["src/migrations/*.ts"],
       }
 );
 AppDataSource.initialize().then(() => {

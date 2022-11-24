d38f3227d13f51096171d3a29172ac2193e0dee1
DavidBassouto <davidbassouto@hotmail.com>
2022-11-24 11:07:34 -0300

    feat: adding cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import "reflect-metadata";
 import express from "express";
+import cors from "cors"
 import "express-async-errors";
 import { handleErrorMiddleware } from "./middlewares/handleError.middleware";
 import { productRoutes } from "./routes/products.routes";
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 app.use("/produtos", productRoutes);
